ffcb36c0d549bb8fe47faff03ac08a1b2b7b77bf
Sean-Fuhrman <stfuhrma@ucsd.edu>
2023-05-24 12:29:19 -0700

    Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+1. 
+Answer: Within a Github action that runs whenever code is pushed 
+Explanation: This ensures automatic testing whenever code it pushed to the github, so it requires no extra steps from the developer and ensures github-level code is tested
+
+2. No
+
+3. No because it requires user interaction so this would be better done with an E2E test.
+   
+4. Yes because you are testing ine small function that doesn't interact with other components. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,85 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('908-555-3333 isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('908-555-3333')).toBe(true);
+});
+
+test('908-444-3233 isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('908-444-3233')).toBe(true);
+});
+
+test('hello isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('hello')).toBe(false);
+});
+
+test('93rufn isPhoneNumber', () => {
+    expect(functions.isPhoneNumber('93rufn')).toBe(false);
+});
+
+test('seantfuhrman@gmail.com isEmail', () => {
+    expect(functions.isEmail('seantfuhrman@gmail.com')).toBe(true);
+});
+
+test('otheremail@gmail.edu isEmail', () => {
+    expect(functions.isEmail('otheremail@gmail.edu')).toBe(true);
+});
+
+test('notAnEmail isEmail', () => {
+    expect(functions.isEmail('notAnEmail')).toBe(false);
+});
+
+test('1234 isEmail', () => {
+    expect(functions.isStrongPassword('1234')).toBe(false);
+});
+
+test('Password123 isStrongPassword', () => {
+    expect(functions.isStrongPassword('Password123')).toBe(true);
+});
+
+test('Password123__ isStrongPassword', () => {
+    expect(functions.isStrongPassword('Password123__')).toBe(true);
+});
+
+test('a isStrongPassword', () => {
+    expect(functions.isStrongPassword('a')).toBe(false);
+});
+
+test('_ isStrongPassword', () => {
+    expect(functions.isStrongPassword('_')).toBe(false);
+});
+
+
+test('12/12/2001 isDate', () => {
+    expect(functions.isDate('12/12/2001')).toBe(true);
+});
+
+test('2/12/2001 isDate', () => {
+    expect(functions.isDate('2/12/2001')).toBe(true);
+});
+
+test('a isDate', () => {
+    expect(functions.isDate('a')).toBe(false);
+});
+
+test('_ isDate', () => {
+    expect(functions.isDate('_')).toBe(false);
+});
+
+test('FFF isHexColor', () => {
+    expect(functions.isHexColor('FFF')).toBe(true);
+});
+
+test('ffffff isHexColor', () => {
+    expect(functions.isHexColor('ffffff')).toBe(true);
+});
+
+test('a isHexColor', () => {
+    expect(functions.isHexColor('a')).toBe(false);
+});
+
+test('_ isHexColor', () => {
+    expect(functions.isHexColor('_')).toBe(false);
+});
+
